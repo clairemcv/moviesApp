f00b7b13623ddbe9b30fbccb9eb3229b8b841206
Claire McVeigh <mcmcveigh@yahoo.com>
2020-07-15 10:10:22 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -5,41 +5,41 @@ export default ({
     movie
 }) => {
     return ( <
-            >
-            <
-            h4 > Overview < /h4> <
-            p > {
-                movie.overview
-            } < /p> <
-            ul className = "list-group list-group-horizontal" >
-            <
-            li key = "ruh"
-            className = "list-group-item list-group-item-dark" >
-            Runtime(min.) <
-            /li> <
-            li key = "rut"
-            className = "list-group-item " > {
-                movie.runtime
-            } <
-            /li> <
-            li key = "rdh"
-            className = "list-group-item list-group-item-dark" >
-            Release Date <
-            /li> <
-            li key = "rdv"
-            className = "list-group-item " > {
-                movie.release_date
-            } <
-            /li> < /
-            ul >
+        >
+        <
+        h4 > Overview < /h4> <
+        p > {
+            movie.overview
+        } < /p> <
+        ul className = "list-group list-group-horizontal" >
+        <
+        li key = "ruh"
+        className = "list-group-item list-group-item-dark" >
+        Runtime(min.) <
+        /li> <
+        li key = "rut"
+        className = "list-group-item " > {
+            movie.runtime
+        } <
+        /li> <
+        li key = "rdh"
+        className = "list-group-item list-group-item-dark" >
+        Release Date <
+        /li> <
+        li key = "rdv"
+        className = "list-group-item " > {
+            movie.release_date
+        } <
+        /li> <
+        /ul>
 
-            <
-            ul className = "list-group list-group-horizontal" >
-            <
-            li key = "gh"
-            className = "list-group-item list-group-item-dark" >
-            Genres <
-            /li> {
+        <
+        ul className = "list-group list-group-horizontal" >
+        <
+        li key = "gh"
+        className = "list-group-item list-group-item-dark" >
+        Genres <
+        /li> {
             movie.genres.map(g => ( <
                 li key = {
                     g.name
@@ -51,40 +51,56 @@ export default ({
             ))
         } <
         /ul> <
-    ul className = "list-group list-group-horizontal" >
+        ul className = "list-group list-group-horizontal" >
         <
         li key = "slh"
-    className = "list-group-item list-group-item-dark" >
+        className = "list-group-item list-group-item-dark" >
         Spoken Languages <
         /li> {
-    movie.spoken_languages.map(lang => ( <
+            movie.spoken_languages.map(lang => ( <
+                li key = {
+                    lang.name
+                }
+                className = "list-group-item" > {
+                    lang.name
+                } <
+                /li>
+            ))
+        } <
+        /ul> <
+        ul className = "list-group list-group-horizontal" >
+        <
+        li key = "pch"
+        className = "list-group-item list-group-item-dark" >
+        Production Companies <
+        /li> {
+            movie.production_companies.map(pc => ( <
+                li key = {
+                    pc.name
+                }
+                className = "list-group-item" > {
+                    pc.name
+                } <
+                /li>
+            ))
+        } <
+        /ul> <
+        ul className = "list-group list-group-horizontal" >
+        <
+        li key = "pct"
+        className = "list-group-item list-group-item-dark" >
+        Production Countries <
+        /li> {movie.production_countries.map(pct => ( <
         li key = {
-            lang.name
+            pct.name
         }
         className = "list-group-item" > {
-            lang.name
+            pct.name
         } <
         /li>
     ))
 } <
 /ul> <
-ul className = "list-group list-group-horizontal" >
-    <
-    li key = "pch"
-className = "list-group-item list-group-item-dark" >
-    Production Companies <
-    /li> {
-movie.production_companies.map(pc => ( <
-li key = {
-    pc.name
-}
-className = "list-group-item" > {
-    pc.name
-} <
-/li>
-))
-} <
-/ul> < /
->
+/>
 );
 };
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -3,49 +3,34 @@ import MovieHeader from "../components/headerMovie";
 import MovieDetails from "../components/movieDetails";
 import "./moviePage.css";
 
-const MoviePage = ({
-    movie
-}) => {
-    return ( <
-        >
-        {
-            movie ? ( <
-                >
-                <
-                MovieHeader movie = {
-                    movie
+const MoviePage = ({ movie }) => {
+  return (
+    <>
+      {movie ? (
+        <>
+          <MovieHeader movie={movie} />
+          <div className="row">
+            <div className="col-3">
+              <img
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                    : "./film-poster-placeholder.png"
                 }
-                /> <
-                div className = "row" >
-                <
-                div className = "col-3" >
-                <
-                img src = {
-                    movie.poster_path ?
-                    `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : "./film-poster-placeholder.png"
-                }
-                className = "movie"
-                alt = {
-                    movie.title
-                }
-                /> < /
-                div > <
-                div className = "col-9" >
-                <
-                MovieDetails movie = {
-                    movie
-                }
-                /> < /
-                div > <
-                /div> < /
-                >
-            ) : ( <
-                p > Waiting
-                for movie details < /p>
-            )
-        } <
-        />
-    );
+                className="movie"
+                alt={movie.title}
+              />
+            </div>
+            <div className="col-9">
+              <MovieDetails movie={movie} />
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Waiting for movie details</p>
+      )}
+    </>
+  );
 };
 
 export default MoviePage;
