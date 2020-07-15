ade9184b0e6db1134bf43e8a9d0ae980b1b434f6
Claire McVeigh <mcmcveigh@yahoo.com>
2020-07-15 17:30:42 +0100

    Add to favorites logic completed; Dummy favorites page

--- src/components/movieList/index.js
+++ src/components/movieList/index.js
@@ -3,18 +3,10 @@ import Movie from "../movieCard/";
 import "./movieList.css";
 
 const MovieList = props => {
-    const movieCards = props.movies.map(m => ( <
-        Movie key = {
-            m.id
-        }
-        movie = {
-            m
-        }
-        />
+    const movieCards = props.movies.map(m => (
+      <Movie key={m.id} movie={m} buttonHandler={props.buttonHandler} />
     ));
-    return <div className = "row movies bg-info" > {
-        movieCards
-    } < /div>;
-};
-
-export default MovieList;
+    return <div className="row movies bg-info">{movieCards}</div>;
+  };
+  
+  export default MovieList;
